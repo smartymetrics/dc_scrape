dd134f44788b79d95addcf8ee1c76d322baec89b
smartychuks <Smirhty@gmail.com>
2026-02-08 12:59:08 +0100

    notif credential

--- hollowscan_app/Constants.js
+++ hollowscan_app/Constants.js
@@ -4,8 +4,8 @@
 const Constants = {
     // ⚠️ 'localhost' does not work on a physical phone.
     // We use your local IP so your phone can talk to your computer.
-    API_BASE_URL: 'https://product.hollowscan.com',// 'https://web-production-18cf1.up.railway.app',
-    // API_BASE_URL: 'http://10.246.149.243:8000',
+    // API_BASE_URL: 'https://product.hollowscan.com',// 'https://web-production-18cf1.up.railway.app',
+    API_BASE_URL: 'http://10.246.149.243:8000',
 
     BRAND: {
         BLUE: '#4F46E5', // Vivid Indigo-Blue
